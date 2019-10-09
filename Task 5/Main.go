2a372bbceade713037116da9997a872067d31cf5
Aliaksei Milto <alexeymilto@gmail.com>
2019-10-09 17:37:43 +0300

    Added parsing resp.Body to file

--- Task 5/Main.go
+++ Task 5/Main.go
@@ -10,19 +10,35 @@ import (
 	"./Link"
 )
 
-func main() {
-	HTMLfilePointer := flag.String("file", "htmlFormat.html",
-		"File which contains paths and destinations in YAML format (default htmlFormat.html)")
-	links, err := Link.ParseLinks(*HTMLfilePointer)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(links)
-	resp, err := http.Get("https://www.google.by")
+func HTMLWriteToFile(url string, filename string) {
+	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-	fmt.Println(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = ioutil.WriteFile(filename, body, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	HTMLfilePointer := flag.String("file", "htmlFormat.html",
+		"File which contains paths and destinations in YAML format (default htmlFormat.html)")
+	_ = flag.String("URL", "https://www.calhoun.io/",
+		"URL, for which need to create sitemap (default https://www.calhoun.io/)")
+	flag.Parse()
+	HTMLWriteToFile("https://www.calhoun.io/", *HTMLfilePointer)
+	links, err := Link.ParseLinks(*HTMLfilePointer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, v := range links {
+		fmt.Println("Href: ", v.Href)
+		fmt.Println("Text: ", v.Text)
+	}
 }
