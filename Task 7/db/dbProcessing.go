863c01e9b4eac98899121356830076934849cf98
Aliaksei Milto <alexeymilto@gmail.com>
2019-10-15 09:32:10 +0300

    Added DbOpen

---
+++ Task 7/db/dbProcessing.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	_ "pq-master" //Driver for PostgreSQL
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "admin"
+	dbName   = "Task7"
+)
+
+func openDB() (*sql.DB, error) {
+	info := fmt.Sprintf("host=%s port=%d user=%s password=%s dbName=%s sslmode=disable",
+		host, port, user, password, dbName)
+	db, err := sql.Open("postgres", info)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db, err
+}
