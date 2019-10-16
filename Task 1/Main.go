a2b63cefb7c1ebd110776abae0e7a3579cc2ddd0
Aliaksei Milto <alexeymilto@gmail.com>
2019-10-16 22:04:13 +0300

    Fixed typo answer

--- Task 1/Main.go
+++ Task 1/Main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv",
-		"a csv file in the format of 'question,anwser' (default problems.csv")
+		"a csv file in the format of 'question,answer' (default problems.csv")
 	limit := flag.Int("limit", 30, "the time for the quiz in seconds (default 30)")
 	flag.Parse()
 	quizQuestions := make(map[string]string)
@@ -33,24 +33,24 @@ func main() {
 		}
 		quizQuestions[record[0]] = record[1]
 	}
-	var problemNum, userAnwser = 1, ""
+	var problemNum, userAnswer = 1, ""
 	for question, correctAnswer := range quizQuestions {
-		anwserCh := make(chan string)
+		answerCh := make(chan string)
 		go func() {
 			fmt.Print("Problem №", problemNum, ": ", question, " = ")
-			_, err := fmt.Scanln(&userAnwser)
+			_, err := fmt.Scanln(&userAnswer)
 			if err != nil {
 				log.Fatalln(err)
 			}
-			anwserCh <- userAnwser
+			answerCh <- userAnswer
 		}()
 		select {
 		case <-timer.C:
 			fmt.Printf("You scored %d of %d \n", problemNum-1, len(quizQuestions)-1)
 			return
-		case userAnwser := <-anwserCh:
-			if userAnwser != correctAnswer {
-				fmt.Println(userAnwser)
+		case userAnswer := <-answerCh:
+			if userAnswer != correctAnswer {
+				fmt.Println(userAnswer)
 				fmt.Println("You scored ", problemNum-1, " of ", len(quizQuestions)-1)
 				return
 			}
