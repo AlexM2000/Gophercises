355f45a08d0b0ac721713894b4d61625d5d51058
Aliaksei Milto <alexeymilto@gmail.com>
2019-10-22 09:19:45 +0300

    Implemented getting PhoneNumbers from database

--- Task 8/db/dbProcessing.go
+++ Task 8/db/dbProcessing.go
@@ -1,8 +1,14 @@
-package db
+package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq" //Driver for postgresql
 )
 
 const (
@@ -21,4 +27,39 @@ func Open() (*sql.DB, error) {
 		panic(err)
 	}
 	return db, err
+}
+
+func startRouter() {
+	r := mux.NewRouter()
+	r.HandleFunc("/getPhoneNums", getPhoneNums).Methods("GET")
+	fmt.Println("Started serving port :8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+func main() {
+	startRouter()
+}
+
+func getPhoneNums(w http.ResponseWriter, r *http.Request) {
+	phoneNums := getPhoneNumsFromDb()
+	json.NewEncoder(w).Encode(phoneNums)
+}
+
+func getPhoneNumsFromDb() []string {
+	dbase, err := Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows, err := dbase.Query("select \"PhoneNum\" from public.\"phone_numbers\" ")
+	phoneNums := make([]string, 0, 8)
+	for rows.Next() {
+		phoneNum := ""
+		err := rows.Scan(&phoneNum)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		phoneNums = append(phoneNums, phoneNum)
+	}
+	return phoneNums
 }
---
+++ Task 8/db/table/Table.go
@@ -0,0 +1,9 @@
+package type
+
+type Table struct {
+	PhoneNum   string
+	FirstName  string
+	SecondName string
+	ThirdName  string
+	ID         int
+}
