96e2e2fedce0b46fa6572ffd5acd8c019883f3a6
Aliaksei Milto <alexeymilto@gmail.com>
2019-10-26 09:11:55 +0300

    Implemented New() func

--- Task 9/deck/Card.go
+++ Task 9/deck/Card.go
@@ -1,24 +1,77 @@
 package deck
 
+type Suit uint8
+
+type Rank uint8
+
 type Card struct {
-	color string
-	value int
+	Suit 
+	Rank
 }
 
-type deck []Card
+const DECKSIZE = 36
+
+const (
+	Spade = iota
+	Diamond
+	Club
+	Heart
+)
+
+suits := []Suit{Spade, Diamond, Club, Heart}
+
+const (
+	Six = iota + 5
+	Seven 
+	Eight
+	Nine
+	Ten
+	Jack
+	Queen
+	King
+	Ace
+)
+
+const (
+	MinRank = Six
+	MaxRank = Ace
+)
+
+type lessFunc func(p1, p2 *Card)
+
+type sortDeck struct {
+	deck []Card
+	less []lessFunc
+}
+
+func generateColor(deck []Card, color string) []Card { 
+}
 
 func New() []Card {
-	return []Card{}
+	newDeck := make([]Card, DECKSIZE, DECKSIZE)
+	for _, suit := range suits {
+		for value := MinRank; value <= MaxRank; value++ {
+			newDeck = append(newDeck, Card{Suit: suit, Rank: value})
+		}
+	}
+	return newDeck
 }
 
-func (d deck) Len() int {
-	return len(d)
+func (d *sortDeck) Len() int {
+	return len(d.deck)
 }
 
-func (d deck) Swap(i, j int) {
-	d[i], d[j] = d[j], d[i]
+func (d *sortDeck) Swap(i, j int) {
+	d.deck[i], d.deck[j] = d.deck[j], d.deck[i]
 }
 
-func (d deck) Less(i, j int) bool {
-	return d[i].value < d[j].value
+func orderedBy(less ...lessFunc) *sortDeck {
+	return &sortDeck{
+		less: less,
+	}
+}
+
+func (d *sortDeck) Less(i, j int) bool {
+	p, q := &d.deck[i], &d.deck[j]
+
 }
