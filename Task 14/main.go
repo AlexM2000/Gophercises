d043480c3140ce5565043922c9358e0ea22d5ed7
Aliaksei Milto <alexeymilto@gmail.com>
2019-11-18 23:26:42 +0300

    Added panic recovery and logging error

---
+++ Task 14/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/panic/", panicDemo)
+	r.HandleFunc("/panic-after/", panicAfterDemo)
+	r.HandleFunc("/", hello)
+	log.Fatal(http.ListenAndServe(":8000", handlerWithPanic(r)))
+}
+
+func handlerWithPanic(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Println(err)
+				http.Error(w, "Error in server", http.StatusInternalServerError)
+			}
+		}()
+		h.ServeHTTP(w, r)
+	})
+}
+
+func panicDemo(w http.ResponseWriter, r *http.Request) {
+	funcThatPanics()
+}
+
+func panicAfterDemo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello!</h1>")
+	funcThatPanics()
+}
+
+func funcThatPanics() {
+	panic("Oh no!")
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "<h1>Hello!</h1>")
+}
